835ba9f600f8d3d97a3790bd550c3da2bd3f566a
Daniel Kosmin <kosmind1@udayton.edu>
2022-08-02 22:00:37 -0400

    comments

--- app/src/main/java/edu/dayton/workoutgenerator/MainActivity.java
+++ app/src/main/java/edu/dayton/workoutgenerator/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        // set listener
         btnWorkout.setOnClickListener(btnWorkoutListener);
     }
 }
--- app/src/main/java/edu/dayton/workoutgenerator/SplashActivity.java
+++ app/src/main/java/edu/dayton/workoutgenerator/SplashActivity.java
@@ -17,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // set splash activity for 5 seconds
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
